dfdeb8589574e5fabc760a6485abb97173ae6d68
sourabh.mahato <sourabh.mahato@mapmyindia.com>
2024-04-22 16:48:09 +0530

    jquery configuration for unit testing

--- src/app/components/dashboard/dashboard.component.spec.ts
+++ src/app/components/dashboard/dashboard.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardComponent } from './dashboard.component';
+import { By } from '@angular/platform-browser';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
@@ -19,5 +20,31 @@ describe('DashboardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set xyz and abc to true and add class to bottom sheet wrapper when sheet is "route"', () => {
+    const sheet = 'route';
+    component.openBottomSheet(sheet);
+
+    // Assert that xyz and abc are set to true
+    expect(component.xyz).toBe(true);
+    expect(component.abc).toBe(true);
+
+    // Assert that the 'show-modal' class is added to the bottom sheet wrapper
+    const bottomSheetWrapper = fixture.debugElement.query(By.css('.bottom-sheet-wrapper'));
+    // expect(bottomSheetWrapper.nativeElement.classList.contains('show-modal')).toBe(true);
+  });
+
+  it('should not change xyz and abc and not add class to bottom sheet wrapper when sheet is not "route"', () => {
+    const sheet = 'some-other-sheet';
+    component.openBottomSheet(sheet);
+
+    // Assert that xyz and abc are not changed
+    expect(component.xyz).toBeFalse();
+    expect(component.abc).toBeFalse();
+
+    // Assert that the 'show-modal' class is not added to the bottom sheet wrapper
+    const bottomSheetWrapper = fixture.debugElement.query(By.css('.bottom-sheet-wrapper'));
+    // expect(bottomSheetWrapper.nativeElement.classList.contains('show-modal')).toBe(false);
   });
 });
--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,12 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
+declare var $: any;
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
 
+export class DashboardComponent {
+  xyz: boolean = false;
+  abc: boolean = false;
+
+  openBottomSheet(sheet: any) {
+    if (sheet == 'route') {
+      this.xyz = true;
+      this.abc = true;
+      $('.bottom-sheet-wrapper').addClass('show-modal');
+    }
+  }
 }
